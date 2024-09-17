10dc834ca4dbf4eba7ddc98009f0c50dced2cba0
LostX228 <abdekr40@gmail.com>
2024-09-18 00:09:26 +0100

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,13 @@
+
+loopy() 
+ function loopy() { 
+ var sWord ="LostX" 
+ while (sWord != "@LostX-@Basic") {// Pass
+ sWord = prompt("𝐄𝐧𝐭𝐞𝐫 𝐓𝐡𝐞 𝐊𝐞𝐲 ;")  
+ } 
+ 
+//confirm("ok");
+
+window.alert("  𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐏𝐀𝐍𝐄𝐋 𝑳𝒐𝒔𝒕𝑿 ");
+
+ } 
